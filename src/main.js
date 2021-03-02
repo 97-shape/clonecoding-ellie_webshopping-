887d828e5ee71313b18bf43a765f1d50cf5e4f62
엄상욱 <sanguookmax@gmail.com>
2021-03-02 14:58:52 +0000

    21.3.2

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
-function loadItems(){
-	return fetch()
+function loadItems() {
+	return fetch('https://raw.githubusercontent.com/97-shape/clonecoding-ellie_webshopping-/master/data/data.json')
+	.then (response => response.json)
+	.then (json => json.items);
 }
+
+function displayItems(items) {
+	const container = document.querySelector('.items');
+	container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+function createHTMLString(item){
+		return `
+		<li class="item">
+			<img class="itemImg"src="https://github.com/97-shape/clonecoding-ellie_webshopping-/blob/master/imgs/${item.image}?raw=true" data-type="${item.type}">
+			<span>${item.gender}, ${item.size}</span>
+		</li>
+`;
+	}
+
+loadItems()
+.then (items => {
+	displayItems(items);
+})
